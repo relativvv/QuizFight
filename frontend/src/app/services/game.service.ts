dfae74a471617da8fb920fcf06b872d9732402f1
Robin Schulte <r.schulte@shopware.com>
2020-12-04 12:31:23 +0100

    added components for gameplay

---
+++ frontend/src/app/services/game.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@angular/core';
+import {TriviaService} from "./trivia.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class GameService {
+
+  constructor(private readonly triviaService: TriviaService) {}
+
+  public generateGameKey() {
+
+  }
+
+  public getOpponent() {
+
+  }
+
+  public getPlayers() {
+
+  }
+
+  public getQuestion() {
+
+  }
+
+  public askQuestion() {
+
+  }
+
+  public dealDamage() {
+
+  }
+}
